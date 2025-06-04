d2d4f203a9b37921617671b764f3eb5ff626c3b6
camiiruiz13 <69661273+camiiruiz13@users.noreply.github.com>
2025-06-04 09:40:51 -0500

    Creacion del proyecto plazoleta

---
+++ src/main/java/com/retoplazoleta/ccamilo/com/microservicioplazoleta/MicroservicioPlazoletaApplication.java
@@ -0,0 +1,13 @@
+package com.retoplazoleta.ccamilo.com.microservicioplazoleta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioPlazoletaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicioPlazoletaApplication.class, args);
+    }
+
+}
