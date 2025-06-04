ca74ee980ffa4cd0981f5052d687709743e2f25a
camiiruiz13 <69661273+camiiruiz13@users.noreply.github.com>
2025-06-04 12:35:57 -0500

    Se agrega dependencias para llamar al token

--- src/main/java/com/retoplazoleta/ccamilo/com/microservicioplazoleta/infraestructure/out/client/impl/GenericAplClient.java
+++ src/main/java/com/retoplazoleta/ccamilo/com/microservicioplazoleta/infraestructure/out/client/impl/GenericAplClient.java
@@ -1,9 +1,13 @@
 package com.retoplazoleta.ccamilo.com.microservicioplazoleta.infraestructure.out.client.impl;
 
+import com.retoplazoleta.ccamilo.com.microservicioplazoleta.infraestructure.input.rest.dto.GenericRequest;
 import com.retoplazoleta.ccamilo.com.microservicioplazoleta.infraestructure.input.rest.dto.GenericResponseDTO;
 import com.retoplazoleta.ccamilo.com.microservicioplazoleta.infraestructure.out.client.IGenericApiClient;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 
 import java.util.LinkedHashMap;
 
@@ -11,6 +15,12 @@ import java.util.LinkedHashMap;
 public class GenericAplClient implements IGenericApiClient {
     @Override
     public GenericResponseDTO<LinkedHashMap<?, ?>> sendRequest(String url, HttpMethod method, Object payload) {
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        ResponseEntity<String> responseEntity;
+        GenericRequest<?> request = new GenericRequest<>();
+
         return null;
     }
 }
